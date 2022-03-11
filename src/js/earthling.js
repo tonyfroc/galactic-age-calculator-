75d7d8997ac59daf43e2cf593bcfbff7b9bc140e
Tony Froccaro <tfroccaro@gmail.com>
2022-03-10 19:32:56 -0500

    Test 8 Green: Confirmed correct return str of how many years an Earthling has exceeded their life expectancy in Venus years

--- src/js/earthling.js
+++ src/js/earthling.js
@@ -1,30 +1,41 @@
 export default class Earthling {
   constructor(earthAge, ethnicity) {
     this.earthAge = earthAge;
-    this.ethnicity = ethnicity; 
+    this.ethnicity = ethnicity;
   }
   lifeExp() {
-    if (this.ethnicity === 'White') {
+    if (this.ethnicity === "White") {
       this.lifeExpectancy = 78;
-    } else if (this.ethnicity === 'Black') {
+    } else if (this.ethnicity === "Black") {
       this.lifeExpectancy = 73;
-    } else if (this.ethnicity === 'Asian') {
+    } else if (this.ethnicity === "Asian") {
       this.lifeExpectancy = 83;
-    } else if (this.ethnicity === 'Latino') {
+    } else if (this.ethnicity === "Latino") {
       this.lifeExpectancy = 80;
-    } else if (this.ethnicity === 'Native American') {
+    } else if (this.ethnicity === "Native American") {
       this.lifeExpectancy = 75;
     }
   }
   mercuryAge() {
-    this.mercuryAge = Math.floor(this.earthAge / .24);
+    this.mercuryAge = Math.floor(this.earthAge / 0.24);
     if (this.mercuryAge > this.lifeExpectancy) {
-      this.mercuryLifeExpectancy = `You have exceeded your Earth life expectancy by ${this.mercuryAge - this.lifeExpectancy} years`;
+      this.mercuryLifeExpectancy = `You have exceeded your Earth life expectancy by ${
+        this.mercuryAge - this.lifeExpectancy
+      } years`;
     } else {
-      this.mercuryLifeExpectancy = `You have about ${this.lifeExpectancy - this.mercuryAge} years left on Mercury!`;
+      this.mercuryLifeExpectancy = `You have about ${
+        this.lifeExpectancy - this.mercuryAge
+      } years left on Mercury!`;
     }
-}
+  }
   venusAge() {
-    this.venusAge = Math.floor(this.earthAge / .62); 
+    this.venusAge = Math.floor(this.earthAge / 0.62);
+    if (this.venusAge > this.lifeExpectancy) {
+      this.venusLifeExpectancy = `You have exceeded your Earth life expectancy by ${
+        this.venusAge - this.lifeExpectancy
+      } years`;
+    } else {
+      this.venusLifeExpectancy = `You have about 10 years left on Venus!`;
+    }
   }
 }
